e3541e2729b846434def1aa77eab98f5072e6c7a
Piotr Witek <72527323+Viciooo@users.noreply.github.com>
2022-05-11 14:23:34 +0200

    changed logging back to test mode

--- src/pages/login_screen/LoginScreen.py
+++ src/pages/login_screen/LoginScreen.py
@@ -153,8 +153,8 @@ def LoginScreen(screen, mainClock):
                     print("Login: ", input_nickname.text, input_password.text, cb_remember.isSelected)
                     # For tests use:
                     if validate():
-                        # if user.login('konto@gmail.com', 'alamakota'):
-                        if user.login(input_nickname.text, input_password.text): # TODO przed prezentacją odkomentować
+                        if user.login('konto@gmail.com', 'alamakota'):
+                        # if user.login(input_nickname.text, input_password.text): # TODO przed prezentacją odkomentować
                             input_nickname.reset()
                             input_password.reset()
                             ChooseCharacter(screen, mainClock, user)
