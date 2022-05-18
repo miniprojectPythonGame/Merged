055e88d25a56c55bf5bc2b1c60b3c40cbe53a22e
atyranski <arkadiusz.tyranski@gmail.com>
2022-05-18 22:35:59 +0200

    Plane and Tavern first view

--- src/pages/tavern/Measurements.py
+++ src/pages/tavern/Measurements.py
@@ -21,13 +21,16 @@ class Measurements:
     text_color = pygame.Color('gray26')
     stat_color = pygame.Color('#35848f')
     white = pygame.Color('white')
-    label_padding = 35
     list_element_width = 650
     list_element_height = 60
     list_element_padding = 15
     hero_title_height = 20
     fight_button_height = 50
     default_color = ColorSchemes()
+
+    plane_padding = 15
+    title_padding = 60
+    header_padding = 30
 
     bt_return = BT_RETURN
 
@@ -45,8 +48,8 @@ class Measurements:
     le_general = {
         "x": margin,
         "y": margin,
-        "width": 650,
-        "height": 60,
+        "width": list_element_width,
+        "height": list_element_height,
         "colors": default_color,
         "property_name": "Level: ",
     }
@@ -54,4 +57,51 @@ class Measurements:
     sl_quests = {
         "x": margin,
         "y": margin,
+    }
+
+    # PLANE: background
+    p_background = {
+        "color": default_color.inactive,
+        "x": window_width - (margin + (window_width - (3 * margin + list_element_width))),
+        "y": margin,
+        "width": window_width - (3 * margin + list_element_width),
+        "height": window_height - 2 * margin,
+    }
+
+    # LABEL: quest title
+    lb_quest_title = {
+        'x': p_background['x'] + plane_padding,
+        'y': p_background['y'] + plane_padding,
+        'anchor': 'topleft',
+        'font': header_secondary_font,
+        'color': white,
+    }
+
+    # LABEL: difficulty
+    lb_difficulty_header = {
+        'x': p_background['x'] + plane_padding,
+        'y': p_background['y'] + plane_padding + title_padding,
+        'anchor': 'topleft',
+        'font': text_font,
+        'color': white,
+    }
+
+    # LABEL: min level
+    lb_min_level_header = {
+        'x': p_background['x'] + plane_padding,
+        'y': p_background['y'] + plane_padding + title_padding + header_padding,
+        'anchor': 'topleft',
+        'font': text_font,
+        'color': white,
+    }
+
+    # BUTTON: fight
+    bt_fight = {
+        "color": ColorSchemes(inactive="#aaaaaa", text_secondary_color="#ffffff"),
+        'x': p_background['x'] + plane_padding,
+        'y': p_background['y'] + p_background['height'] - (plane_padding + 40),
+        "width": p_background['width'] - 2 * plane_padding,
+        "height": 40,
+        "text": 'Fight',
+        "border-radius": 10,
     }
--- src/pages/tavern/Tavern.py
+++ src/pages/tavern/Tavern.py
@@ -5,17 +5,42 @@ from pygame.locals import *
 from src.components.Button import Button
 from src.components.Label import Label
 from src.components.ListElement import ListElement
+from src.components.ScrollableList import ScrollableList
+from src.components.Plane import Plane
 
 from .Measurements import Measurements as meas
-
-from src.components.ScrollableList import ScrollableList
-
 from src.globals.const_values import getDifficulty
 
 
 def Tavern(screen, mainClock, user):
+
+    def reloadPreview(activeQuest):
+        p_background = Plane(meas.p_background['x'], meas.p_background['y'],
+                             meas.p_background['width'], meas.p_background['height'],
+                             meas.p_background['color'], screen)
+
+        lb_quest_title = Label(activeQuest.name, meas.lb_quest_title['font'], meas.lb_quest_title['color'],
+                               screen, meas.lb_quest_title['x'], meas.lb_quest_title['y'])
+
+        lb_difficulty_header = Label("Difficulty: ", meas.lb_difficulty_header['font'],
+                                     meas.lb_difficulty_header['color'], screen,
+                                     meas.lb_difficulty_header['x'], meas.lb_difficulty_header['y'])
+
+        lb_min_level_header = Label("Min level: ", meas.lb_min_level_header['font'],
+                                     meas.lb_min_level_header['color'], screen,
+                                     meas.lb_min_level_header['x'], meas.lb_min_level_header['y'])
+
+        bt_fight = Button(meas.bt_fight['color'], meas.bt_fight['x'], meas.bt_fight['y'],
+                          meas.bt_fight['width'], meas.bt_fight['height'], screen,
+                          meas.bt_fight['text'], meas.header_tertiary_font,
+                          border_radius=meas.bt_fight['border-radius'])
+
+        return p_background, lb_quest_title, bt_fight, lb_difficulty_header, lb_min_level_header
+
     showHand = False
-    isRunning = True
+    running = True
+    hero = user.currentHero
+    activeQuest = None
 
     quest_list = user.currentHero.quests.quest_list
 
@@ -33,17 +58,30 @@ def Tavern(screen, mainClock, user):
                                          meas.le_general['y'] + 0 * meas.list_element_padding + 0 * meas.list_element_height,
                                          meas.le_general['width'], meas.le_general['height'],
                                          meas.le_general['colors'], screen, quest.name, difficulty,
-                                         meas.le_general['property_name'], "12",
+                                         "", "",
                                          object=quest))
 
-    sl_heroes = ScrollableList(meas.sl_quests['x'], meas.sl_quests['y'], screen, list_elements,
+    sl_quests = ScrollableList(meas.sl_quests['x'], meas.sl_quests['y'], screen, list_elements,
                                meas.list_element_height, meas.list_element_padding)
+
+    p_background = None
+    lb_quest_title = None
+    ml_quest_description = None
+    lb_min_level_header = None
+    lb_min_leevl_value = None
+    lb_difficulty_header = None
+    lb_difficulty_value = None
+    lb_gold_header = None
+    lb_gold_value = None
+    lb_teasure_header = None
+    bt_fight = None
+
 
     displayedContent = [
         label_page, bt_return,
     ]
 
-    while isRunning:
+    while running:
         screen.fill((255, 255, 255))
         if showHand:
             pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
@@ -53,7 +91,14 @@ def Tavern(screen, mainClock, user):
         for elem in displayedContent:
             elem.draw()
 
-        sl_heroes.draw()
+        sl_quests.draw()
+
+        if activeQuest is not None:
+            p_background.draw()
+            lb_quest_title.draw()
+            bt_fight.draw()
+            lb_difficulty_header.draw()
+            lb_min_level_header.draw()
 
         mx, my = pygame.mouse.get_pos()
 
@@ -73,13 +118,24 @@ def Tavern(screen, mainClock, user):
                         isRunning = False
                         print("Redirecting: ArmorShop.py -> CityMap.py")
 
+                    # if activeQuest is not None and bt_fight.rect.collidepoint(event.pos):
+                    #     Fight(screen, mainClock, hero, activeQuest)
+                    #     break
+
+                    for questLine in sl_quests.list:
+                        if questLine.rect.collidepoint(event.pos):
+                            activeQuest = questLine.object
+                            print(activeQuest)
+                            p_background, lb_quest_title, bt_fight, lb_difficulty_header, lb_min_level_header = reloadPreview(activeQuest)
+                            break
+
                 # SCROLL
                 if event.button == 4:
-                    sl_heroes.onScrollUp()
+                    sl_quests.onScrollUp()
                     break
 
                 elif event.button == 5:
-                    sl_heroes.onScrollDown()
+                    sl_quests.onScrollDown()
                     break
 
         pygame.display.update()
